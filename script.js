07e52d0b0cf128923af7f2eb75cb02c367e8aee5
KainTheDev <thica.doi@gmail.com>
2024-04-26 21:45:58 +0700

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- // Function to open invite link
- function openInvite(type) {
+// Function to open invite link
+function openInvite(type) {
     const url = type === 'bot' ? 'https://discord.com/oauth2/authorize?client_id=1229494317115244544&permissions=8&scope=bot' : 'https://discord.gg/veal';
     const name = type === 'bot' ? 'Invite bot' : 'Join server';
     window.open(url, name, 'width=500,height=700,left=500,top=50');
@@ -9,29 +9,15 @@
     // Get references to DOM elements
     const botAvatar = document.getElementById('veal-avatar');
     const cardsContainer = document.getElementById('cards');
-    const title = document.getElementById('title')
     const selectCategories = document.getElementById('selectCategories')
     const commandsSearchBar = document.getElementById('commandsSearchBar')
-    const locationHref = window.location.href.split('/')
-    const titleChange = locationHref[locationHref.length - 1].split('.')[0]
-    title.innerText = title.innerText.replace('{page}', titleChange)
     const buttonsList = document.getElementById('buttonsList')
     const commandsTotal = document.getElementById('commandsTotal')
     const copyButton = document.getElementsByName('copyButton')
-    const commandModal = document.getElementById('modalBody')
-
-    // Fetch commands data from JSON file
-    const response = await fetch('./json/commands.json')
-    const commandsJSON = await response.json()
-
-    // Map commands data and add index
-    const commands = commandsJSON.map((command, index) => ({ ...command, index: index + 1 }))
-
-    // Extract categories from commands data
-    const categories = ["all", ...new Set(commands.map(command => command.category))];
+    const codeCopyButton = document.getElementById('codeCopyButton')
 
     // Function to add options to select element
-    function addSelectOptions() {
+    function addSelectOptions(categories) {
         for (const category of categories) {
             selectCategories.innerHTML += `<option>${category}</option>`
         }
@@ -41,7 +27,7 @@
     function createCommandCard(command) {
         const { name, description, category, usage, index } = command;
         return `
-        <div class="card m-3 shiny-hover rounded-5 py-3" style="width: 400px;">
+        <div class="card m-3 shiny-hover rounded-5 py-3 bg-transparent bg-gradient" style="width: 400px;">
             <div class="card-body">
                 <h5 class="card-title fw-normal">${index}. <span class="fw-bold">${name}</span></h5>
                 <p class="fs-6 fw-light pt-2">${description}</p>
@@ -88,10 +74,20 @@
 
     // Check current page and add event listeners accordingly
     if (window.location.href.includes('commands')) {
+        // Fetch commands data from JSON file
+        const response = await fetch('./json/commands.json')
+        const commandsJSON = await response.json()
+
+        // Map commands data and add index
+        const commands = commandsJSON.map((command, index) => ({ ...command, index: index + 1 }))
+
+        // Extract categories from commands data
+        const categories = ["all", ...new Set(commands.map(command => command.category))];
+
         // Display total number of commands
         commandsTotal.innerText = commands.length
 
-        addSelectOptions()
+        addSelectOptions(categories)
         if (Array.isArray(commands) && commands.length > 0) renderCommandCards(commands);
 
         selectCategories.addEventListener('change', () => {
@@ -119,18 +115,20 @@
 
         commandsSearchBar.addEventListener("input", updateCards);
 
-        const cards = cardsContainer.childNodes
-        for (const card of cards) {
-            card.addEventListener('click', ({ target }) => {
-                if (!target.className.includes('card')) return;
-                const modal = new bootstrap.Modal(document.getElementById('commandModal'))
-                commandModal.innerHTML = card.innerHTML
-               modal.show()
-            })
-        }
-
     } else if (window.location.href.includes('homepage')) {
         initializeMediaQueryListener();
+    } else if (window.location.href.includes('authorization/spotify') && window.location.href.includes('?code=')) {
+        const code = window.location.href.split("?code=")[1]
+        if(!code || code.length < 290) return window.location.href = '/homepage.html';
+        const originalIcon = '<i class="fa-solid fa-copy"></i>'
+        codeCopyButton.addEventListener('click', () => {
+            codeCopyButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+            setTimeout(() => {
+                codeCopyButton.innerHTML = originalIcon
+            }, 1500)
+        })
+    } else {
+        window.location.href = "/homepage.html"
     }
 
     // Initialize Bootstrap tooltips
